7025008f42e2d7562d167a1665f1f3b876dfcd2a
Binh <binh.phan@takeaway.com>
2021-03-09 20:07:41 +0100

    updated PetAdapter

--- app/src/main/java/com/riceplant/petadoption/PetAdapter.kt
+++ app/src/main/java/com/riceplant/petadoption/PetAdapter.kt
@@ -1,24 +1,47 @@
 package com.riceplant.petadoption
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.riceplant.petadoption.data.Pet
 
-class PetAdapter : RecyclerView.Adapter<>() {
+class PetAdapter(private val mPets: List<Pet>) : RecyclerView.Adapter<PetAdapter.PetViewholder>() {
 
-    class PetViewholder(val view: View): RecyclerView.ViewHolder(view) {
+    inner class PetViewholder(view: View) : RecyclerView.ViewHolder(view) {
+        private val petImageView: ImageView = view.findViewById(R.id.pet_image)
+        private val nameTextView: TextView = view.findViewById(R.id.pet_name)
+        private val breedTextView: TextView = view.findViewById(R.id.pet_breed)
+        private val sexTextView: TextView = view.findViewById(R.id.pet_sex)
+        private val descriptionTextView: TextView = view.findViewById(R.id.pet_description)
+        private var currentPet: Pet? = null
 
+        fun bind(pet: Pet) {
+            currentPet = pet
+
+            petImageView.setImageResource(pet.photo)
+            nameTextView.text = pet.name
+            breedTextView.text = pet.breed
+            sexTextView.text = pet.sex
+            descriptionTextView.text = pet.description
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ??? {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PetAdapter.PetViewholder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
 
-    override fun onBindViewHolder(holder: ???, position: Int) {
-        TODO("Not yet implemented")
+        return PetViewholder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return mPets.size
+    }
+
+    override fun onBindViewHolder(holder: PetViewholder, position: Int) {
+        val pet = mPets.get(position)
+        holder.bind(pet)
     }
 }
--- app/src/main/java/com/riceplant/petadoption/data/Pet.kt
+++ app/src/main/java/com/riceplant/petadoption/data/Pet.kt
@@ -1,4 +1,4 @@
 package com.riceplant.petadoption.data
 
-data class Pet(var name: String, var breed: String, var photo: Int, var description: String) {
+data class Pet(var name: String, var breed: String, var photo: Int, var description: String, var sex: String) {
 }
